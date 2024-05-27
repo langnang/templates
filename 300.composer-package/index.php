7b4133c084c3c3610c9c7eae53d4fe30293444db
langnang <langnang.chen@outlook.com>
2024-05-27 18:04:59 +0800

    2024-05-27 18:04:56

---
+++ 300.composer-package/index.php
@@ -0,0 +1,11 @@
+<?php
+
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
+
+dump($dotenv);
+var_dump($_ENV);
+var_dump($_SERVER);
