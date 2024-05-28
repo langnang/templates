1485b65f47c330976cea8ec5fda16a1d6a7874dc
langnang <langnang.chen@outlook.com>
2024-05-28 10:00:25 +0800

    composer-package

--- 300.composer-package/index.php
+++ 300.composer-package/index.php
@@ -3,9 +3,19 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+
+/** 载入配置支持 */
+if (!file_exists(__DIR__ . '/.env')) {
+  file_exists('./install.php') ? header('Location: install.php') : print ('Missing Config File');
+  exit;
+}
+
+
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
 
 dump($dotenv);
-var_dump($_ENV);
-var_dump($_SERVER);
+dump($_ENV);
+dump($_SERVER);
+
+dump(get_declared_classes());
