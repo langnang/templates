24ac4eb2b4f1b8740336d8eb7fa55f99ea7855e8
langnang <langnang.chen@outlook.com>
2024-06-05 10:23:48 +0800

    2024-06-05 10:23:47

--- 302.laravel8-modules/modules/Home/Resources/views/home/master/content-item.blade.php
+++ 302.laravel8-modules/modules/Home/Resources/views/home/master/content-item.blade.php
@@ -3,19 +3,37 @@
 @section('content')
   <div class="container">
     <h1>Content</h1>
-    @isset($content)
-      <div class="row">
-        <div class="col-8">
-          <div class="card">
-            <div class="card-header">
-              <div class="card-title mb-0">{{ $content['title'] }}</div>
-            </div>
-            <div class="card-body">
-              {!! markdown_to_html($content['text']) !!}
-            </div>
+    <div class="row">
+      <div class="col-8">
+        <div class="card">
+          <div class="card-header p-2">
+            <div class="card-title mb-0">{{ $content['title'] }}</div>
+          </div>
+          <div class="card-body">
+            {!! markdown_to_html($content['text']) !!}
           </div>
         </div>
       </div>
-    @endisset
+      <div class="col-4">
+        <div class="card mb-3">
+          <div class="card-header p-2">
+            <div class="card-title mb-0">关连分类</div>
+          </div>
+          <div class="card-body"></div>
+        </div>
+        <div class="card mb-3">
+          <div class="card-header p-2">
+            <div class="card-title mb-0">关连标签</div>
+          </div>
+          <div class="card-body"></div>
+        </div>
+        <div class="card mb-3">
+          <div class="card-header p-2">
+            <div class="card-title mb-0">关连链接</div>
+          </div>
+          <div class="card-body"></div>
+        </div>
+      </div>
+    </div>
   </div>
 @endsection
