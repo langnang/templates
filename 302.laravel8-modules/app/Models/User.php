e34255060911cd14da9206e19db7af19019c1f2e
langnang <langnang.chen@outlook.com>
2024-06-13 18:56:59 +0800

    2024-06-13 18:56:47

--- 302.laravel8-modules/app/Models/User.php
+++ 302.laravel8-modules/app/Models/User.php
@@ -11,7 +11,9 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+    protected $table = "users";
 
+    protected $primaryKey = 'uid';
     /**
      * The attributes that are mass assignable.
      *
