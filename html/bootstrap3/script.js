bd89557cfdaa4ff74d9966cf07052566b4b7e014
langnang <langnang.chen@outlook.com>
2024-08-22 20:35:07 +0800

    2024-08-22 20:35:05

---
+++ html/bootstrap3/script.js
@@ -0,0 +1,4 @@
+
+window.jQuery || document.write('<scrip src="https://unpkg.com/jquery@3.7.1/dist/jquery.slim.min.js"><\/script>')
+
+window.Mock || document.write('<scrip src="https://unpkg.com/mockjs@1.1.0/dist/mock-min.js"><\/script>')
